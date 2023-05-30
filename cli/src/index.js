8d74f6e6d77fa26c1714955759a5511d0d7777cc
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-05-30 00:23:43 -0400

    Refactor a bit

--- cli/src/index.js
+++ cli/src/index.js
@@ -6,7 +6,7 @@ import { pathExists, readJson, remove } from 'fs-extra/esm';
 import { createRequire } from 'node:module';
 import { dirname, join } from 'node:path';
 import lockfile from 'proper-lockfile';
-import resolvePackagePath from 'resolve-package-path';
+import resolvePackageManifestPath from 'resolve-package-path';
 
 const require = createRequire(import.meta.url);
 const debug = Debug('sync-pnpm');
@@ -14,8 +14,6 @@ const debug = Debug('sync-pnpm');
 const syncDir = './dist';
 
 export default async function syncPnpm(dir = process.cwd()) {
-  console.log(dir);
-
   const root = await findRoot(dir);
   const ownPackageJson = await readJson(join(dir, 'package.json'));
   const ownDependencies = [
@@ -32,33 +30,68 @@ export default async function syncPnpm(dir = process.cwd()) {
   );
 
   for (const pkg of packagesToSync) {
+    let name = pkg.packageJson.name;
+
+    /**
+     * This likely won't happen, but we can't have declared
+     * a dependency on a package without a name.
+     */
+    if (!name) continue;
+
     const syncFrom = join(pkg.dir, syncDir);
-    const resolvedPackagePath = dirname(
-      resolvePackagePath(pkg.packageJson.name, dir)
-    );
+    const resolvedPackagePath = resolvePackagePath(name, dir);
+
     const syncTo = join(resolvedPackagePath, syncDir);
 
-    if (await pathExists(syncFrom)) {
-      let releaseLock;
+    await syncPkg(syncFrom, syncTo);
+  }
+}
 
-      try {
-        releaseLock = await lockfile.lock(syncTo, { realpath: false });
-        debug(`lockfile created for syncing to ${syncTo}`);
-      } catch (e) {
-        debug(
-          `lockfile already exists for syncing to ${syncTo}, some other sync process is already handling this directory, so skipping...`
-        );
-        continue;
-      }
+/**
+ * @param {string} name
+ * @param {string} startingDirectory resolve from here
+ */
+function resolvePackagePath(name, startingDirectory) {
+  const resolvedManifestPath = resolvePackageManifestPath(
+    name,
+    startingDirectory
+  );
 
-      if (await pathExists(syncTo)) {
-        await remove(syncTo);
-        debug(`removed ${syncTo} before syncing`);
-      }
+  if (!resolvedManifestPath) {
+    throw new Error(`Could not find package, ${name}`);
+  }
 
-      debug(`syncing from ${syncFrom} to ${syncTo}`);
-      await hardLinkDir(syncFrom, [syncTo]);
-      releaseLock();
+  const resolvedPackagePath = dirname(resolvedManifestPath);
+
+  return resolvedPackagePath;
+}
+
+/**
+ * @param {string} syncFrom
+ * @param {string} syncTo
+ */
+async function syncPkg(syncFrom, syncTo) {
+  if (await pathExists(syncFrom)) {
+    let releaseLock;
+
+    try {
+      releaseLock = await lockfile.lock(syncTo, { realpath: false });
+      debug(`lockfile created for syncing to ${syncTo}`);
+    } catch (e) {
+      debug(
+        `lockfile already exists for syncing to ${syncTo}, some other sync process is already handling this directory, so skipping...`
+      );
+
+      return;
     }
+
+    if (await pathExists(syncTo)) {
+      await remove(syncTo);
+      debug(`removed ${syncTo} before syncing`);
+    }
+
+    debug(`syncing from ${syncFrom} to ${syncTo}`);
+    await hardLinkDir(syncFrom, [syncTo]);
+    releaseLock();
   }
 }
