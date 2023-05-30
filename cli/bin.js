8949a6d9bf674eae40287790518cf9f5b3d18420
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-05-30 00:00:26 -0400

    Initial extraction from CS

---
+++ cli/bin.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+import run from './src/index.js';
+
+let yarg = yargs(hideBin(process.argv));
+
+yarg.wrap(yarg.terminalWidth());
+
+yarg
+  .command(
+    ['run', '$0'],
+    'the default command -- sync dependencies if relevant',
+    () => {},
+    (args) => {
+      return run(args);
+    }
+  )
+  .help().argv;
---
+++ cli/src/index.js
@@ -0,0 +1,62 @@
+import { findRoot } from '@manypkg/find-root';
+import { getPackages } from '@manypkg/get-packages';
+import { hardLinkDir } from '@pnpm/fs.hard-link-dir';
+import Debug from 'debug';
+import { pathExists, readJson, remove } from 'fs-extra/esm';
+import { createRequire } from 'node:module';
+import { dirname, join } from 'node:path';
+import lockfile from 'proper-lockfile';
+import resolvePackagePath from 'resolve-package-path';
+
+const require = createRequire(import.meta.url);
+const debug = Debug('sync-pnpm');
+
+const syncDir = './dist';
+
+export default async function syncPnpm(dir = process.cwd()) {
+  const root = await findRoot(dir);
+  const ownPackageJson = await readJson(join(dir, 'package.json'));
+  const ownDependencies = [
+    ...Object.keys(ownPackageJson.dependencies ?? {}),
+    ...Object.keys(ownPackageJson.devDependencies ?? {}),
+  ];
+
+  const localPackages = (await getPackages(root.rootDir)).packages;
+
+  const packagesToSync = localPackages.filter(
+    (p) =>
+      p.packageJson.name !== 'sync-pnpm' &&
+      ownDependencies.includes(p.packageJson.name)
+  );
+
+  for (const pkg of packagesToSync) {
+    const syncFrom = join(pkg.dir, syncDir);
+    const resolvedPackagePath = dirname(
+      resolvePackagePath(pkg.packageJson.name, dir)
+    );
+    const syncTo = join(resolvedPackagePath, syncDir);
+
+    if (await pathExists(syncFrom)) {
+      let releaseLock;
+
+      try {
+        releaseLock = await lockfile.lock(syncTo, { realpath: false });
+        debug(`lockfile created for syncing to ${syncTo}`);
+      } catch (e) {
+        debug(
+          `lockfile already exists for syncing to ${syncTo}, some other sync process is already handling this directory, so skipping...`
+        );
+        continue;
+      }
+
+      if (await pathExists(syncTo)) {
+        await remove(syncTo);
+        debug(`removed ${syncTo} before syncing`);
+      }
+
+      debug(`syncing from ${syncFrom} to ${syncTo}`);
+      await hardLinkDir(syncFrom, [syncTo]);
+      releaseLock();
+    }
+  }
+}
